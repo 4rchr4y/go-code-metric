c9918b34b0634efc9aee6076630d5a6c5129501e
EnthusedFear <enthusedfir@gmail.com>
2023-11-08 16:15:47 +0200

    abstractness calculation refactoring

--- pdq/abstractness.go
+++ pdq/abstractness.go
@@ -1,23 +1,19 @@
 package pdq
 
 import (
-	"errors"
+	"fmt"
 )
 
 func CalcAbstractness(abstractElementsNum int, concreteElementsNum int) (float64, error) {
-	if abstractElementsNum < 0 {
-		return -1, errors.New("the number of abstract elements must not be a negative number")
+	if abstractElementsNum < 0 || concreteElementsNum < 0 {
+		return -1, fmt.Errorf("invalid metric value: abstractElementsNum %v, concreteElementsNum %v; values must not be less than 0", abstractElementsNum, concreteElementsNum)
 	}
 
-	if concreteElementsNum < 0 {
-		return -1, errors.New("the number of concrete elements must not be a negative number")
+	if concreteElementsNum == 0 {
+		return float64(abstractElementsNum), nil
 	}
 
-	if abstractElementsNum == 0 || concreteElementsNum == 0 {
-		return 0, nil
-	}
-
-	abstractness := float64(abstractElementsNum) / (float64(abstractElementsNum) + float64(concreteElementsNum))
+	abstractness := float64(abstractElementsNum) / float64(concreteElementsNum)
 
 	return abstractness, nil
 }
--- pdq/abstractness_test.go
+++ pdq/abstractness_test.go
@@ -1,42 +1,92 @@
 package pdq
 
 import (
-	"errors"
+	"fmt"
 	"math"
 	"testing"
 )
 
 func TestAbstractness(t *testing.T) {
-	tests := []struct {
-		name                string
-		abstractElementsNum int
-		concreteElementsNum int
-		expected            float64
-		expectedError       error
-	}{
-		{"PositiveNumbers", 10, 20, 0.3333333333333333, nil},
-		{"ZeroAbstractNumber", 0, 20, 0, nil},
-		{"ZeroConcreteNumber", 10, 0, 0, nil},
-		{"NegativeAbstractNumber", -1, 1, -1, errors.New("the number of abstract elements must not be a negative number")},
-		{"NegativeConcreteNumber", 1, -1, -1, errors.New("the number of concrete elements must not be a negative number")},
-		{"BothNumbersNegative", -10, -20, -1, errors.New("the number of abstract elements must not be a negative number")},
-		{"LargeNumbers", math.MaxInt32, math.MaxInt32, 0.5, nil},
-		{"AbstractGreaterThanConcrete", 30, 10, 0.75, nil},
-		{"ConcreteGreaterThanAbstract", 10, 30, 0.25, nil},
-		{"EqualNumbers", 20, 20, 0.5, nil},
-	}
 
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			got, err := CalcAbstractness(tc.abstractElementsNum, tc.concreteElementsNum)
+	t.Run("valid: input 1", func(t *testing.T) {
+		expected := 3.0
+		got, _ := CalcAbstractness(30, 10)
 
-			if got != tc.expected {
-				t.Fatalf("%v failed: results don't match. CalcAbstractness(%v, %v) == %v. Expected: %v", tc.name, tc.abstractElementsNum, tc.concreteElementsNum, got, tc.expected)
-			}
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
 
-			if err != nil && err.Error() != tc.expectedError.Error() {
-				t.Fatalf("%v failed: errors don't match. Error: %v. Expected error: %v", tc.name, err, tc.expectedError)
-			}
-		})
-	}
+	t.Run("valid: input 2", func(t *testing.T) {
+		expected := 0.3333333333333333
+		got, _ := CalcAbstractness(10, 30)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("valid: input 3", func(t *testing.T) {
+		expected := 1.0
+		got, _ := CalcAbstractness(20, 20)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("valid: input 4", func(t *testing.T) {
+		expected := 0.0
+		got, _ := CalcAbstractness(0, 20)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("valid: input 5", func(t *testing.T) {
+		expected := 10.0
+		got, _ := CalcAbstractness(10, 0)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("valid: both values are math.MaxInt32", func(t *testing.T) {
+		expected := 1.0
+		got, _ := CalcAbstractness(math.MaxInt32, math.MaxInt32)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+	})
+
+	t.Run("Invalid: input -1, 1", func(t *testing.T) {
+		expected := -1.0
+		expectedErr := fmt.Errorf("invalid metric value: abstractElementsNum %v, concreteElementsNum %v; values must not be less than 0", -1, 1)
+		got, err := CalcAbstractness(-1, 1)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+
+		if err.Error() != expectedErr.Error() {
+			t.Errorf("errors don't match: error (%v), expectedErr (%v)", err, expectedErr)
+		}
+	})
+
+	t.Run("Invalid: input 1, -1,", func(t *testing.T) {
+		expected := -1.0
+		expectedErr := fmt.Errorf("invalid metric value: abstractElementsNum %v, concreteElementsNum %v; values must not be less than 0", 1, -1)
+		got, err := CalcAbstractness(1, -1)
+
+		if got != expected {
+			t.Errorf("got %v, expected %v", got, expected)
+		}
+
+		if err.Error() != expectedErr.Error() {
+			t.Errorf("errors don't match: error (%v), expectedErr (%v)", err, expectedErr)
+		}
+	})
 }
