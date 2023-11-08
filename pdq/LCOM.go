fce5f6bc15ce719b56abed954283a070623fbb89
EnthusedFear <enthusedfir@gmail.com>
2023-11-08 19:29:22 +0200

    joined LCOM calculations in one file

--- pdq/LCOM.go
+++ pdq/LCOM.go
@@ -15,3 +15,33 @@ func CalcLCOM(nonSharedFields int, sharedFields int) (int, error) {
 
 	return 0, nil
 }
+
+func CalcLCOM96b(attributesNum int, methodsNum int, methodsPerAttribute []int) (float64, error) {
+	if attributesNum < 0 || methodsNum < 0 {
+		return -1, fmt.Errorf("attributesNum %v, methodsNum %v; values must be >= 0", attributesNum, methodsNum)
+	}
+
+	if attributesNum == 0 || methodsNum == 0 {
+		return 0, nil
+	}
+
+	if attributesNum != len(methodsPerAttribute) {
+		return -1, fmt.Errorf("attributesNum: %v != methodsPerAttribute length: %v", attributesNum, len(methodsPerAttribute))
+	}
+
+	methodsDifferenceSum := 0
+	for i, methodsForAttribute := range methodsPerAttribute {
+		if methodsForAttribute < 0 {
+			return -1, fmt.Errorf("invalid number of methods for attribute at index %d: %d; should be >= 0", i, methodsForAttribute)
+		}
+		if methodsForAttribute > methodsNum {
+			return -1, fmt.Errorf("too many methods for attribute at index %d: %d; should be <= methodsNum: %d", i, methodsForAttribute, methodsNum)
+		}
+
+		methodsDifferenceSum += methodsNum - methodsForAttribute
+	}
+
+	lcom96b := float64(methodsDifferenceSum) / (float64(attributesNum * methodsNum))
+
+	return lcom96b, nil
+}
--- pdq/LCOM_test.go
+++ pdq/LCOM_test.go
@@ -78,3 +78,115 @@ func TestCalcLCOM(t *testing.T) {
 		require.Error(t, err)
 	})
 }
+
+func TestCalcLCOM96b(t *testing.T) {
+	t.Run("valid: input 1", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{3, 1, 1})
+
+		assert.Equal(t, 0.4444444444444444, got)
+		require.NoError(t, err)
+	})
+	t.Run("valid: input 2", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{1, 1, 1})
+
+		assert.Equal(t, 0.6666666666666666, got)
+		require.NoError(t, err)
+	})
+	t.Run("valid: input 3", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{2, 2, 1})
+
+		assert.Equal(t, 0.4444444444444444, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: input 4", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{2, 1, 2})
+
+		assert.Equal(t, 0.4444444444444444, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: input 5", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{1, 2, 2})
+
+		assert.Equal(t, 0.4444444444444444, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: all methods accesses attributes", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{3, 3, 3})
+
+		assert.Equal(t, 0.0, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: no method accesses attributes", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{0, 0, 0})
+
+		assert.Equal(t, 1.0, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: low num of attributes and high num of methods", func(t *testing.T) {
+		got, err := CalcLCOM96b(1, 9, []int{9})
+
+		assert.Equal(t, 0.0, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: no attributes", func(t *testing.T) {
+		got, err := CalcLCOM96b(0, 3, []int{})
+
+		assert.Equal(t, 0.0, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("valid: no methods", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 0, []int{})
+
+		assert.Equal(t, 0.0, got)
+		require.NoError(t, err)
+	})
+
+	t.Run("invalid: attribute count mismatch", func(t *testing.T) {
+		got, err := CalcLCOM96b(2, 3, []int{1, 2, 1})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+
+	t.Run("invalid: negative attributes", func(t *testing.T) {
+		got, err := CalcLCOM96b(-1, 3, []int{})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+
+	t.Run("invalid: negative methods", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, -1, []int{})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+
+	t.Run("invalid: negative value in methods per attribute slice 1", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{-1, 0, 0})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+
+	t.Run("invalid: negative value in methods per attribute slice 2", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{0, 0, -1})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+
+	t.Run("invalid: invalid methods per attribute exceeds total", func(t *testing.T) {
+		got, err := CalcLCOM96b(3, 3, []int{99, 1, 1})
+
+		assert.Equal(t, -1.0, got)
+		require.Error(t, err)
+	})
+}
