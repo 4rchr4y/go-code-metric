ba54429127816ff510471b64e9aaa00917ce21da
EnthusedFear <enthusedfir@gmail.com>
2023-11-14 16:06:21 +0200

    changed AMC variable name

--- pdq/AMC.go
+++ pdq/AMC.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func CalcAMC(methodComplexity int, totalMethodsNum int) (float64, error) {
-	if methodComplexity < 0 || totalMethodsNum < 0 {
-		return 0, fmt.Errorf("method complexity and total number of methods cannot be negative, but got %d, %d", methodComplexity, totalMethodsNum)
+func CalcAMC(methodsComplexity int, totalMethodsNum int) (float64, error) {
+	if methodsComplexity < 0 || totalMethodsNum < 0 {
+		return 0, fmt.Errorf("method complexity and total number of methods cannot be negative, but got %d, %d", methodsComplexity, totalMethodsNum)
 	}
 
-	if methodComplexity == 0 || totalMethodsNum == 0 {
+	if methodsComplexity == 0 || totalMethodsNum == 0 {
 		return 0, nil
 	}
 
-	return float64(methodComplexity) / float64(totalMethodsNum), nil
+	return float64(methodsComplexity) / float64(totalMethodsNum), nil
 }
